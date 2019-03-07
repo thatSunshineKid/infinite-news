925e44596765f4e50f9a19f6949343611f916c8b
Adam Sunshine <adam@csuac.com>
2019-03-07 15:01:18 -0700

    added first gql client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "https://newsflashback.herokuapp.com/graphql/"
+});
+// const client = new ApolloClient({
+//   uri: "http://localhost:3080/graphql/"
+// });
+
+
+client
+  .query({
+    query: gql`
+      {
+        allPosts {
+          id
+          title
+          author {
+            user {
+              firstName
+              lastName
+            }
+          }
+          url
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
