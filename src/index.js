33c33f3a21f41b0578aa64da3de6dfc887b9eeb1
Adam Sunshine <adam@csuac.com>
2019-03-11 16:06:21 -0600

    got graphql query to display in react component properly

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: "https://newsflashback.herokuapp.com/graphql/"
@@ -13,28 +14,40 @@ const client = new ApolloClient({
 //   uri: "http://localhost:3080/graphql/"
 // });
 
+function HomeComponent() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  )
+}
+// Test query to make sure graphql is hooked up properly
+// client
+//   .query({
+//     query: gql`
+//       {
+//         allPosts {
+//           id
+//           title
+//           author {
+//             user {
+//               firstName
+//               lastName
+//             }
+//           }
+//           url
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
-client
-  .query({
-    query: gql`
-      {
-        allPosts {
-          id
-          title
-          author {
-            user {
-              firstName
-              lastName
-            }
-          }
-          url
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+ReactDOM.render(
+  <HomeComponent />,
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  document.getElementById('root')
+
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
