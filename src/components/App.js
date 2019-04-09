85c7055da4553564dc6001f3d86eef3eb886ef76
Adam Sunshine <adam@csuac.com>
2019-04-09 14:24:08 -0600

    added flexbox/bootstrap to app for layout styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import PostList from './PostList'
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
-      return <PostList />
+    return (
+    <Grid fluid>
+      <Row>
+        <Col xs={12} md={6}>
+          <PostList />
+        </Col>
+      </Row>
+    </Grid>
+      );
   }
 }
 
